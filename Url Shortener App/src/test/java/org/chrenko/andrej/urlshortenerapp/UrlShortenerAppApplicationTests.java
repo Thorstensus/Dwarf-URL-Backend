d97463fc46b8add5bb3460c5db7ccc44e84e976a
Thorstensus <achren1@gmail.com>
2024-03-04 15:15:12 +0100

    created base project structure

---
+++ Url Shortener App/src/test/java/org/chrenko/andrej/urlshortenerapp/UrlShortenerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chrenko.andrej.urlshortenerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
